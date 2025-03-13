4f1caa2e26a53f83b4abb37ab0a52d5ed7349819
Myeongseok Baek <codetemplate@hanmail.net>
2025-03-13 17:37:31 +0900

    project created

---
+++ src/main/java/pe/msbaek/tdd_workshop/TddWorkshopApplication.java
@@ -0,0 +1,13 @@
+package pe.msbaek.tdd_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/msbaek/tdd_workshop/TddWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.msbaek.tdd_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
